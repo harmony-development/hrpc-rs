dc5c833c3aef1ab0aeee0c864fa47059bc809598
yusdacra <yusdacra@users.noreply.github.com>
2021-02-15 14:52:33 +0000

    deploy: 2dba3283a9efc69330c9281f7227cff3dcd93a62

--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hrpc"] = [{"text":"impl Sync for Client","synthetic":true,"types":[]},{"text":"impl&lt;Msg, Resp&gt; Sync for Socket&lt;Msg, Resp&gt;","synthetic":true,"types":[]},{"text":"impl Sync for ClientError","synthetic":true,"types":[]},{"text":"impl Sync for InvalidUrlKind","synthetic":true,"types":[]},{"text":"impl&lt;Msg&gt; Sync for SocketMessage&lt;Msg&gt;","synthetic":true,"types":[]}];
+implementors["hrpc"] = [{"text":"impl Sync for ClientError","synthetic":true,"types":[]},{"text":"impl Sync for InvalidUrlKind","synthetic":true,"types":[]},{"text":"impl Sync for Client","synthetic":true,"types":[]},{"text":"impl&lt;Msg&gt; Sync for SocketMessage&lt;Msg&gt;","synthetic":true,"types":[]},{"text":"impl&lt;Msg, Resp&gt; Sync for Socket&lt;Msg, Resp&gt;","synthetic":true,"types":[]}];
 implementors["hrpc_build"] = [{"text":"impl Sync for Builder","synthetic":true,"types":[]}];
-implementors["interop"] = [{"text":"impl Sync for Ping","synthetic":true,"types":[]},{"text":"impl Sync for Pong","synthetic":true,"types":[]},{"text":"impl Sync for MuClient","synthetic":true,"types":[]}];
+implementors["interop"] = [{"text":"impl Sync for MuClient","synthetic":true,"types":[]},{"text":"impl Sync for Ping","synthetic":true,"types":[]},{"text":"impl Sync for Pong","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
