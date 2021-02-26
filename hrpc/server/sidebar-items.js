e7fff5f67d5aa4604741dbdb3d864226b67cdab9
yusdacra <yusdacra@users.noreply.github.com>
2021-02-26 05:16:42 +0000

    deploy: e15ab52afd50818c22a576146313027018fcbfaf

--- hrpc/server/sidebar-items.js
+++ hrpc/server/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["StatusCode","An HTTP status code (`status-code` in RFC 7230 et al.)."]],"trait":[["CustomError","Trait that needs to be implemented to use an error type with a generated service server."]]});
+initSidebarItems({"fn":[["json_err_bytes","Creates a JSON error response from a message."]],"struct":[["StatusCode","An HTTP status code (`status-code` in RFC 7230 et al.)."]],"trait":[["CustomError","Trait that needs to be implemented to use an error type with a generated service server."]]});
