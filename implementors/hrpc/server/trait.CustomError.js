8ca2c891ddcb7fcc9e0f8beb9b839e37a2f4f6f2
yusdacra <yusdacra@users.noreply.github.com>
2021-04-07 20:55:52 +0000

    deploy: b9261c3cf7ac2f5bf4048838c65e4173dc6732f3

--- implementors/hrpc/server/trait.CustomError.js
+++ implementors/hrpc/server/trait.CustomError.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["interop"] = [{"text":"impl CustomError for ServerError","synthetic":false,"types":[]}];
+implementors["interop"] = [{"text":"impl <a class=\"trait\" href=\"hrpc/server/trait.CustomError.html\" title=\"trait hrpc::server::CustomError\">CustomError</a> for <a class=\"enum\" href=\"interop/enum.ServerError.html\" title=\"enum interop::ServerError\">ServerError</a>","synthetic":false,"types":["interop::ServerError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
