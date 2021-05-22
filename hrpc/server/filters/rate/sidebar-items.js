03a5cba6ea0845dfe2f3e1e4cbb94245ba47d95b
yusdacra <yusdacra@users.noreply.github.com>
2021-05-22 23:28:38 +0000

    deploy: 677449424b285a9167d6d119418ff52cfc32b5a1

---
+++ hrpc/server/filters/rate/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["rate_limit","Creates a filter that will return an `Err(error)` with `error` generated with the provided error function if rate limited."]],"struct":[["Rate","A rate of requests per time period."],["State",""]]});
