6517063d24890714965096effefd650a1aa8d1b2
yusdacra <yusdacra@users.noreply.github.com>
2021-10-14 11:02:05 +0000

    deploy: 1905ee088883b0b5456efaacd0d21f85ec40ec9a

--- interop/sidebar-items.js
+++ interop/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ServerError",""]],"fn":[["client",""],["main",""],["server",""]],"mod":[["mu_client","Generated client implementations."],["mu_server","Generated server implementations."]],"struct":[["Ping","Ping message."],["Pong","Pong message."],["Server",""]]});
+initSidebarItems({"enum":[["ServerError",""]],"fn":[["client",""],["main",""],["server",""]],"mod":[["mu_client","Generated client implementations."],["mu_server","Generated server implementations."]],"struct":[["MuService",""],["Ping","Ping message."],["Pong","Pong message."]]});
