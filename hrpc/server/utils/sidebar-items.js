b21df3fed7f2c5377fb93b4d028c8c79f3eb2734
yusdacra <yusdacra@users.noreply.github.com>
2021-10-18 17:14:54 +0000

    deploy: 8f1c6875a0d53d9a0cbd6a1c3d4e2e34783cc40c

--- hrpc/server/utils/sidebar-items.js
+++ hrpc/server/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["serve","Start serving a server on the specified address."]],"trait":[["HeaderMapExt","Helper methods for working with `HeaderMap`."]]});
+initSidebarItems({"fn":[["recommended_layers","A set of “recommended” layers, namely:"],["serve","Start serving a server on the specified address."]],"trait":[["HeaderMapExt","Helper methods for working with `HeaderMap`."]]});
