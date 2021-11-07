dc1ca14c3af68a1188d89e84345e5c7d9dbbc310
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-11-07 18:31:33 +0300

    feat: expose SocketMessage and common::socket module

--- crates/hrpc/src/common/mod.rs
+++ crates/hrpc/src/common/mod.rs
@@ -1,5 +1,4 @@
 pub(crate) mod buf;
-pub(crate) mod socket;
 
 /// Extension type used by [`crate::Request`] and [`crate::Response`].
 pub mod extensions;
@@ -7,3 +6,5 @@ pub mod extensions;
 pub mod fut;
 /// Common code to work with transports.
 pub mod transport;
+/// Common code to work with sockets.
+pub mod socket;
