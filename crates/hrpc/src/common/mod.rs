07e6e403c3bc2fa43fd0cbe00c116d5ded57dc66
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-11-10 21:01:32 +0300

    fix: feature gate socket and transport modules in common

--- crates/hrpc/src/common/mod.rs
+++ crates/hrpc/src/common/mod.rs
@@ -5,6 +5,8 @@ pub mod extensions;
 /// Common future types used by `hrpc-rs`.
 pub mod fut;
 /// Common code to work with sockets.
+#[cfg(any(feature = "client", feature = "server"))]
 pub mod socket;
 /// Common code to work with transports.
+#[cfg(any(feature = "client", feature = "server"))]
 pub mod transport;
