40989c69a62e50f2f56ba9f2a68720260b2cc38a
yusdacra <yusdacra@users.noreply.github.com>
2021-11-10 17:20:52 +0000

    deploy: c3d06d639097f8204b1afa3a9ef0a365127c6651

--- hrpc/server/transport/http/sidebar-items.js
+++ hrpc/server/transport/http/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["utils","Utilities for working with this transport."]],"struct":[["Hyper","A transport based on [`hyper`]."]]});
+initSidebarItems({"mod":[["utils","Utilities for working with this transport."]],"struct":[["Hyper","A transport based on [`hyper`] that supports TLS."]]});
