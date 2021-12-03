746304030e2681de2c3262a47d8b6bac0c38b44f
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-12-03 23:28:37 +0300

    feat(common): make modify layer take anything that implements Fn, as opposed to fn pointers

--- crates/hrpc/src/common/layer/modify.rs
+++ crates/hrpc/src/common/layer/modify.rs
@@ -9,52 +9,55 @@ use tower::{Layer, Service};
 
 use crate::{request::BoxRequest, response::BoxResponse};
 
-/// Function to modify a request.
-pub type ModifyReq = fn(&mut BoxRequest);
-/// Function to modify a response.
-pub type ModifyResp = fn(&mut BoxResponse);
-
 /// Layer for creating [`Modify`] instances.
 #[derive(Clone)]
-pub struct ModifyLayer {
+pub struct ModifyLayer<ModifyReq, ModifyResp> {
     req_fn: ModifyReq,
     resp_fn: ModifyResp,
 }
 
-impl ModifyLayer {
+impl<ModifyReq, ModifyResp> ModifyLayer<ModifyReq, ModifyResp> {
     /// Create a new layer.
     pub fn new(req_fn: ModifyReq, resp_fn: ModifyResp) -> Self {
         Self { req_fn, resp_fn }
     }
+}
 
+impl<ModifyReq> ModifyLayer<ModifyReq, fn(&mut BoxResponse)> {
     /// Create a new layer that only modifies requests.
     pub fn new_request(f: ModifyReq) -> Self {
         Self::new(f, |_| ())
     }
+}
 
+impl<ModifyResp> ModifyLayer<fn(&mut BoxRequest), ModifyResp> {
     /// Create a new layer that only modifies responses.
     pub fn new_response(f: ModifyResp) -> Self {
         Self::new(|_| (), f)
     }
 }
 
-impl<S> Layer<S> for ModifyLayer {
-    type Service = Modify<S>;
+impl<ModifyReq, ModifyResp, S> Layer<S> for ModifyLayer<ModifyReq, ModifyResp>
+where
+    ModifyReq: Clone,
+    ModifyResp: Clone,
+{
+    type Service = Modify<ModifyReq, ModifyResp, S>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        Modify::new(inner, self.req_fn, self.resp_fn)
+        Modify::new(inner, self.req_fn.clone(), self.resp_fn.clone())
     }
 }
 
 /// Service that lets you modify / inspect requests and responses.
 #[derive(Clone)]
-pub struct Modify<S> {
+pub struct Modify<ModifyReq, ModifyResp, S> {
     inner: S,
     req_fn: ModifyReq,
     resp_fn: ModifyResp,
 }
 
-impl<S> Modify<S> {
+impl<ModifyReq, ModifyResp, S> Modify<ModifyReq, ModifyResp, S> {
     /// Create a new service by wrapping a given service.
     pub fn new(inner: S, req_fn: ModifyReq, resp_fn: ModifyResp) -> Self {
         Self {
@@ -65,15 +68,17 @@ impl<S> Modify<S> {
     }
 }
 
-impl<S> Service<BoxRequest> for Modify<S>
+impl<ModifyReq, ModifyResp, S> Service<BoxRequest> for Modify<ModifyReq, ModifyResp, S>
 where
     S: Service<BoxRequest, Response = BoxResponse>,
+    ModifyReq: Fn(&mut BoxRequest),
+    ModifyResp: Fn(&mut BoxResponse) + Clone,
 {
     type Response = BoxResponse;
 
     type Error = S::Error;
 
-    type Future = ModifyFuture<S::Future>;
+    type Future = ModifyFuture<ModifyResp, S::Future>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Service::poll_ready(&mut self.inner, cx)
@@ -84,23 +89,24 @@ where
 
         ModifyFuture {
             fut: Service::call(&mut self.inner, req),
-            resp_fn: self.resp_fn,
+            resp_fn: self.resp_fn.clone(),
         }
     }
 }
 
 pin_project! {
     /// Future for [`Modify`].
-    pub struct ModifyFuture<Fut> {
+    pub struct ModifyFuture<ModifyResp, Fut> {
         #[pin]
         fut: Fut,
         resp_fn: ModifyResp,
     }
 }
 
-impl<Fut, Err> Future for ModifyFuture<Fut>
+impl<ModifyResp, Fut, Err> Future for ModifyFuture<ModifyResp, Fut>
 where
     Fut: Future<Output = Result<BoxResponse, Err>>,
+    ModifyResp: Fn(&mut BoxResponse),
 {
     type Output = Result<BoxResponse, Err>;
 
