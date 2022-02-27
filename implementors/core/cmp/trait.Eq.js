d27d658a9e8ecb1cd4930cf3fa79fd86c8735651
yusdacra <yusdacra@users.noreply.github.com>
2022-02-27 19:31:40 +0000

    deploy: b9d227dc7cc8f428398a694399b797546280a272

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["hrpc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"hrpc/proto/enum.HrpcErrorIdentifier.html\" title=\"enum hrpc::proto::HrpcErrorIdentifier\">HrpcErrorIdentifier</a>","synthetic":false,"types":["hrpc::proto::HrpcErrorIdentifier"]}];
+implementors["hrpc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"hrpc/proto/enum.HrpcErrorIdentifier.html\" title=\"enum hrpc::proto::HrpcErrorIdentifier\">HrpcErrorIdentifier</a>","synthetic":false,"types":["hrpc::proto::HrpcErrorIdentifier"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
